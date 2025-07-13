e22e13c39f7e6cf3d05235b55bf7ad55bc958447
MD. Zabir Hasan <hasanjabir627@gmail.com>
2025-07-14 00:24:15 +0600

    Add files via upload

---
+++ logo3.py
@@ -0,0 +1,21 @@
+from manim import *
+
+class logo3(Scene):
+    def construct(self):
+        
+        github_logo = SVGMobject("github-mark.svg")
+        github_logo.set_height(2)
+        github_logo.set_fill(WHITE, opacity=1)            
+        github_logo.set_stroke(WHITE, width=4)            
+
+       
+        self.play(FadeIn(github_logo, scale=0.8))
+        self.wait(1)
+
+        
+        name_text = Text("Manim Projects", font="JetBrainsMono NF", font_size=64)
+        name_text.move_to(github_logo.get_center())
+
+        # Transform logo into name
+        self.play(Transform(github_logo, name_text))
+        self.wait(2)
